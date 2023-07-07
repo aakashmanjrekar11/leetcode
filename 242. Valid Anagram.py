c4141f31fe77e33dbaf4deb9ccf8de537c7b6d73
Aakash Manjrekar <aakashmanjrekar@gmail.com>
2023-07-06 22:40:08 -0400

    modified 242

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,3 +1,6 @@
+
+# The solution has a time complexity of O(n), where n is the length of the input strings. This is because the solution iterates over the characters of both strings once to count their occurrences and then compares the counts.
+
 class Solution:
 	def isAnagram(self, s: str, t: str) -> bool:
         
@@ -6,17 +9,18 @@ class Solution:
 			return False
 		
 		# initialize empty hashmaps for each strings
-		countS, countT = {}, {}
+		count_S, count_T = {}, {}
 
 		# build the hashmaps
+		# For each character at index i, it increments the count for that character in the respective dictionary using count_S.get(s[i], 0) and count_T.get(t[i], 0). The get method returns the count of the character if it exists in the dictionary or 0 if it doesn't.
 		for i in range(len(s)):
-			countS[s[i]] = 1 + countS.get(s[i], 0)
-			countT[t[i]] = 1 + countT.get(t[i], 0)
+			count_S[s[i]] = 1 + count_S.get(s[i], 0)
+			count_T[t[i]] = 1 + count_T.get(t[i], 0)
 		
 		# iterate through both hashmaps and check if count for each key is same
-        # c is the key for the character in the hashmap
-		for c in countS:
-			if countS[c] != countT.get(c, 0):
+        # current_char is the index for keys in the hashmap
+		for current_char in count_S:
+			if count_S[current_char] != count_T.get(current_char, 0):
 				return False
 		
 		# otherwise, both hashmaps are same, t is anagram of s
